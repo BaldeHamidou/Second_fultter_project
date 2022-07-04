5d1d901bb202c63d74a275567e868b8b0efcb144
BaldeHamidou <hamidou2balde@yahoo.fr>
2022-07-05 01:32:27 +0200

    difficult commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,33 +12,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        // home: MyHomePage(title: 'Flutter Demo Home Page'),
-        //ListviewRandomColor listviewtests = ListviewRandomColor();
-        home: ListView(
-          children: [
-            for (var i = 0; i < 30; i++)
-              LimitedBox(
-                maxHeight: 200,
-                child: Container(
-                  //color:
-                  color: Color(Random().nextInt(0xffffffff)),
-                ),
-              ),
-          ],
-        ));
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: _randomColorListview,
+    );
   }
 }
 
@@ -126,3 +115,18 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+/* *************************************************************************************************************** */
+/* ***********************************************  PERSONAL WIDGET ***********************************************/
+Widget _randomColorListview = ListView(
+  children: [
+    for (var i = 0; i < 30; i++)
+      LimitedBox(
+        maxHeight: 200,
+        child: Container(
+          //color:
+          color: Color(Random().nextInt(0xffffffff)),
+        ),
+      ),
+  ],
+);
